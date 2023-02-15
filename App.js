3fb551a6b55cf1d9b5453320ebb5d53d64f77074
Dare <edidiongekrikpo94@gmail.com>
2023-02-15 07:19:28 +0100

    appjs

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from "react";
-import {View} from 'react-native'
-import Home from "./components/Home";
-import Details from "./components/Details";
-import Liked from "./components/Liked";
+import React from 'react';
+import {View, Text} from 'react-native'
+import Home from './components/Home';
+import Details from './components/Details';
+import Liked from './components/Liked';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const stack = createNativeStackNavigator();
+const stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={Home}/>
+      <Tab.Screen name='Liked' component={Liked}/>
+      <Tab.Screen name='Profile' component={Profile}/>
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
